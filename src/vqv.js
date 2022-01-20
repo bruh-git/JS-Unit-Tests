147aa6c14479ceee99d2ab9a6ab7c74a4082bd06
Bruna <brunac.cardososilva@gmail.com>
2022-01-20 00:29:33 -0300

    Requisito 3
    Concluido.

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,12 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, idade) => {
+  if (!name || !idade) { return undefined; }
+return `Oi, meu nome é ${name}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
